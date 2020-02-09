ad27c42e6a5ea02ed2987f7b33a19f4cfc766fc6
k-ashu <ashutosh.k1809@gmail.com>
2020-02-09 11:01:17 +0530

    added passowrd hashing using bcrypt

--- src/routers/task.js
+++ src/routers/task.js
@@ -48,7 +48,10 @@ router.patch('/tasks/:id', async (req, res) => {
     }
 
     try {
-        const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const tast = await Task.findById(req.params.id)
+        update.forEach(update => task[update]=req.body[update]);
+        tast.save()
+        // const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
 
         if (!task) {
             return res.status(404).send()
